34553f20344a50ac358c48f3d567e5f14018218a
Tobias Blom <tobias.blom@techne-dev.se>
2014-07-16 09:18:49 +0200

    - Tweaks, clean-ups, and fixes

--- archinstall-bootstrap.sh
+++ archinstall-bootstrap.sh
@@ -23,20 +23,31 @@ ARCH=armv6h
 # Change to the mirror which best fits for you
 MIRROR="http://mirror.archlinuxarm.org"
 
+addCustomRepo() {
+    [[ $(grep -c arch-rpi-techne "${RFS}/etc/pacman.conf" || true) -eq 0 ]] && {
+	cat >> "${RFS}/etc/pacman.conf" <<EOF
+[arch-rpi-techne]
+Server = http://www.techne-dev.se/arch-rpi-techne
+EOF
+    }
+
+    return 0
+}
+
 ###############################################################################
 #
 #  Variables
 #
 FIRST_PACKAGE=(filesystem)
 BASH_PACKAGES=(glibc ncurses readline bash)
-PACMAN_PACKAGES=(acl archlinux-keyring attr bzip2 coreutils curl e2fsprogs expat gnupg gpgme keyutils gcc-libs krb5 libarchive libassuan libgpg-error libgcrypt libssh2 lzo2 openssl pacman xz zlib)
+PACMAN_PACKAGES=(acl archlinux-keyring attr bzip2 coreutils curl e2fsprogs expat gnupg gpgme keyutils gcc-libs krb5 libarchive libassuan libgpg-error libgcrypt libssh2 lzo2 openssl pacman xz zlib shadow)
 
 # EXTRA_PACKAGES=(pacman-mirrorlist tar libcap arch-install-scripts util-linux systemd)
 PACKAGES=(${FIRST_PACKAGE[*]} ${BASH_PACKAGES[*]} ${PACMAN_PACKAGES[*]})
 
 LIST=$(mktemp archinstall.XXXXXXXXXX)
-CHROOT_DIR=archinstall-chroot
-DIR=archinstall-pkg
+RFS=RFS
+PKGS=pkgs
 
 ###############################################################################
 #
@@ -45,8 +56,8 @@ DIR=archinstall-pkg
 cleanUp() {
     rm -f ${LIST};
     for MOUNT_POINT in "dev/pts" "dev" "sys" "proc"; do
-	[[ -z $(mount | grep "${CHROOT_DIR}/${MOUNT_POINT}") ]] || \
-	    umount "${CHROOT_DIR}/${MOUNT_POINT}"
+	[[ -z $(mount | grep "${RFS}/${MOUNT_POINT}") ]] || \
+	    umount "${RFS}/${MOUNT_POINT}"
     done
 }
 
@@ -54,9 +65,27 @@ trap cleanUp EXIT SIGHUP SIGINT SIGTERM
 
 ###############################################################################
 #
+#  Functions
+#
+copyResolvConf() {
+    [ -f "/etc/resolv.conf" ] && {
+	mkdir -p "${RFS}/etc/";
+	install -m0644 "/etc/resolv.conf" "${RFS}/etc/resolv.conf"
+    }
+    return 0
+}
+
+setupMirrorList() {
+    mkdir -p "${RFS}/etc/pacman.d/";
+    echo "Server = ${MIRROR}/${ARCH}/\$repo" >> "${RFS}/etc/pacman.d/mirrorlist"
+    return 0
+}
+
+###############################################################################
+#
 #  Main script
 #
-mkdir -p "${DIR}" "${CHROOT_DIR}"
+mkdir -p "${PKGS}" "${RFS}"
 
 # Create a list of filenames for the arch packages
 wget -q -O- "${MIRROR}/${ARCH}/core/" | sed -n "s|.*href=\"\\([^\"]*xz\\)\".*|\\1|p" >> ${LIST}
@@ -64,29 +93,34 @@ wget -q -O- "${MIRROR}/${ARCH}/core/" | sed -n "s|.*href=\"\\([^\"]*xz\\)\".*|\\
 # Download and extract each package
 for PACKAGE in ${PACKAGES[*]}; do
     FILE=$(grep "$PACKAGE-[0-9]" ${LIST} | head -n1)
-    wget "${MIRROR}/${ARCH}/core/${FILE}" -c -O "${DIR}/${FILE}"
-    xz -dc "${DIR}/$FILE" | tar x -k -C "${CHROOT_DIR}"
-    rm -f "${CHROOT_DIR}/.PKGINFO" "${CHROOT_DIR}/.MTREE" "${CHROOT_DIR}/.INSTALL"
+    [[ -f "${PKGS}/${FILE}" ]] || wget "${MIRROR}/${ARCH}/core/${FILE}" -c -O "${PKGS}/${FILE}"
+    xz -dc "${PKGS}/$FILE" | tar x -k -C "${RFS}"
+    rm -f "${RFS}/.PKGINFO" "${RFS}/.MTREE" "${RFS}/.INSTALL"
 done
 
 # Create mount points
-mount -t proc proc "${CHROOT_DIR}/proc/"
-mount -t sysfs sys "${CHROOT_DIR}/sys/"
-mount -o bind /dev "${CHROOT_DIR}/dev/"
-mkdir -p "${CHROOT_DIR}/dev/pts"
-mount -t devpts pts "${CHROOT_DIR}/dev/pts/"
+mount -t proc proc "${RFS}/proc/"
+mount -t sysfs sys "${RFS}/sys/"
+mount -o bind /dev "${RFS}/dev/"
+mkdir -p "${RFS}/dev/pts"
+mount -t devpts pts "${RFS}/dev/pts/"
 
 # Prepare pacman installation
-[ -f "/etc/resolv.conf" ] && cp "/etc/resolv.conf" "${CHROOT_DIR}/etc/"
-
-mkdir -p "${CHROOT_DIR}/etc/pacman.d/"
-echo "Server = ${MIRROR}/${ARCH}/\$repo" >> "${CHROOT_DIR}/etc/pacman.d/mirrorlist"
+copyResolvConf
+setupMirrorList
 
 # Setup, update, and install pacman. Clean up package cache
-chroot ${CHROOT_DIR} pacman-key --init
-chroot ${CHROOT_DIR} pacman-key --populate archlinux
-chroot ${CHROOT_DIR} pacman -Syu --noconfirm pacman --force
-chroot ${CHROOT_DIR} pacman --noconfirm -Sc
+chroot "${RFS}" pacman-key --init
+chroot "${RFS}" pacman-key --populate archlinux
+addCustomRepo
+chroot "${RFS}" pacman -Syu --noconfirm pacman --force
 
-# Restore file after pacman installation
-[ -f "/etc/resolv.conf" ] && cp "/etc/resolv.conf" "${CHROOT_DIR}/etc/"
+# Restore files after installation
+for FILE in etc/pacman.conf.pacorig etc/resolv.conf.pacorig etc/pacman.d/mirrorlist.pacorig; do
+    rm -f "${RFS}/${FILE}"
+done
+copyResolvConf
+
+# Clean up
+addCustomRepo
+chroot "${RFS}" pacman --noconfirm -Sc
